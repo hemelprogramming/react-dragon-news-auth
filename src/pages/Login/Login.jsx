7654eb686d21173c5afded2fb2b80337d61d5401
hemelprogramming <himelahmed1442@gmail.com>
2024-04-04 23:29:01 +0600

    added navbar section

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>this is login</h1>
+    </div>
+  );
+};
+
+export default Login;
